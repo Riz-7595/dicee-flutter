a76834a10220e3a6787fe3da7ee3c0f8c74ea46b
Riz-7595 <rizwankhalid7595@gmail.com>
2023-09-17 23:38:20 +0500

    alligned dice

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,26 @@ void main() {
 class Dicee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.red,
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset("images/dice1.png"),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset("images/dice2.png"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
